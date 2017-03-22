1ee2400d7d234df65f6bc2ba5e9cd76d21d6adf4
lalzz <lalzz@users.noreply.github.com>
2017-03-22 14:23:20 -0700

    Basics

--- src/main/java/net/chromaryu/discordgaming/dungeon/DungeonGenerator.java
+++ src/main/java/net/chromaryu/discordgaming/dungeon/DungeonGenerator.java
@@ -1,7 +1,20 @@
 package net.chromaryu.discordgaming.dungeon;
 
-/**
- * Created by midgard on 17/03/06.
- */
+import java.util.Random;
+
 public class DungeonGenerator {
+  static String seed = "";
+  static Random gen = new Random(seed);
+  
+  public static void setSeed(String s)
+  {
+    this.seed = s;
+    this.gen = new Random(seed);
+  }
+  
+  public static Dungeon generateDungeon()
+  {
+    Dungeon d = new Dungeon();
+    return d;
+  }
 }
