11d172ff36c2f103d8c6a43a9e083b6740a0c556
Lalith Katta <lalzz@users.noreply.github.com>
2017-04-29 20:53:02 -0700

    Update DungeonGenerator.java

--- src/main/java/net/chromaryu/discordgaming/dungeon/DungeonGenerator.java
+++ src/main/java/net/chromaryu/discordgaming/dungeon/DungeonGenerator.java
@@ -2,10 +2,16 @@ package net.chromaryu.discordgaming.dungeon;
 
 import java.util.Random;
 
+
 public class DungeonGenerator {
   static String seed = "";
   static Random gen = new Random(seed);
-  
+  private final static char BLANK = ' ';
+  private static final int MAXROW = 12;
+  private static final int MAXCOL = 12;
+  private int myMaxRow;
+  private int myMaxCol;
+  private char [][] myMaze;
   public static void setSeed(String s)
   {
     this.seed = s;
@@ -20,64 +26,11 @@ public class DungeonGenerator {
   }
   public static boolean testValid(Dungeon d)
   {
-    import java.io.File;
-import java.util.Scanner;
-import java.io.IOException;
 
-/**
- * Description of the Class
- *
- * @author Administrator
- * @created July 16, 2002
- */
-class ThreadTheMaze{
- /**
-  * Description of the Field
-  */
-  private final static char BLANK = ' ';
-  private static final int MAXROW = 12;
-  private static final int MAXCOL = 12;
-  private int myMaxRow;
-  private int myMaxCol;
-  private char [][] myMaze;
-
-  public ThreadTheMaze(){
-   myMaze = new char [MAXROW + 1][MAXCOL + 1];
-   myMaxRow = myMaze.length - 1;
-   myMaxCol = myMaze[0].length - 1;
-  }
-
-  /**
-   * Initiates the trace process
-   *
-   * @param none
-   */
   public void doTraceMaze() {
    loadMaze();
    traceMaze(myMaze, myMaxRow/2, myMaxCol/2);
   }
-
-  /**
-   * Loads the maze characters from mazeData.txt
-   *
-   * @param maze Description of Parameter
-   */
-  private void loadMaze(){
-   String line;
-   Scanner in;
-   try{
-       in = new Scanner(new File("mazeData.txt"));
-
-       for (int row = 1; row <= myMaxRow; row++){
-              line = in.nextLine();
-              for (int col = 1; col <= myMaxCol; col++){
-              myMaze[row][col] = line.charAt(col-1);
-              }
-       }
-   }catch(IOException i){
-   System.out.println("Error: " + i.getMessage());
-  }
-}
 
   /**
    * Description of the Method
@@ -97,19 +50,6 @@ class ThreadTheMaze{
     System.out.println("Hit enter to see if there are more solutions.");
     String anything = console.nextLine();
   }
-
-  /**
-   * Will attempt to find a path out of the maze. A
-   * path will be marked with the ! marker. The method 
-   * makes a copy of the array each time so that only 
-   * the path out will be marked, otherwise extra ! 
-   * markers will appear in the answer.
-   * The function is recursive.
-   *
-   * @param maze Description of Parameter
-   * @param row Description of Parameter
-   * @param col Description of Parameter
-   */
   public void traceMaze(char[][] maze, int row, int col){
     //char[][] mazeCopy = (char[][])maze.clone();
 
